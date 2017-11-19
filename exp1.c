821f7a3a5a4a34e832a4a9790fb84923bef183e2
aswinavofficial <aswinavofficial@gmail.com>
2017-11-19 08:21:52 +0000

    Add files via upload

--- exp1.c
+++ exp1.c
@@ -1,63 +1,50 @@
 #include<stdio.h>
 #include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
 #include<semaphore.h>
-#include<ctype.h>
-#include<sys/types.h>
-#include<sys/wait.h>
-#define philosopher 5
-#define left (philosopher-1)%philosopher
-#define right philosopher+1%philosopher
-
-pthread_t chopstick[philosopher];
-sem_t ph[philosopher];
-
+#include<unistd.h>
+# define philosopher 5
+sem_t chopstick[philosopher];
+pthread_t ph[philosopher];
+void think(int n)
+{
+printf("\nPhilosopher %d is thinking\n",n);
+sleep(1);
+}
+void hungry(int n)
+{
+  printf("\nPhilosopher %d is hungry\n",n);
+  sleep(1);
+}
 
 void eat(int n)
 {
-printf("\nPhilosopher %d is eating \n ",n);
-sleep(1);
+  printf("\nPhilosopher %d is eating\n",n);
+  sleep(1);
+
 }
-
-void think(int n)
-{
-printf("\nPhilosopher %d is thinking \n",n);
-sleep(1);
-}
-
-void hungry(int n)
-{
-printf("\nPhilosopher %d is hungry\n",n);
-sleep(1);
-}
-
-
-
 void *philo(int n)
 {
-while(1)
-{
+while (1 ){
 think(n);
 hungry(n);
-sem_wait(&ph[left]);
-sem_wait(&ph[right]);
+int left=(n-1)%philosopher;
+int right=(n+1)%philosopher;
+sem_wait(&chopstick[left]);
+sem_wait(&chopstick[right]);
 eat(n);
-
-sem_post(&ph[left]);
-sem_post(&ph[right]);
+sem_post(&chopstick[left]);
+sem_post(&chopstick[right]);
 think(n);
 }
-}
 
-int main()
+}
+void main()
 {
 int i;
 for(i=0;i<philosopher;i++)
-sem_init(&ph[i],0,1);
+sem_init(&chopstick[i],0,1);
 
 for(i=0;i<philosopher;i++)
-pthread_create(&chopstick[i],0,(void *) philo,(void *)i);
-
+pthread_create(&ph[i],0,(void *) philo,(void *)i);
 while(1);
 }
